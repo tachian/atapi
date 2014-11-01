6dc0be89e6cbe81660713f18fd9c5a8aadc1e1c2
André Tachian <andre.tachian@veduca.com.br>
2014-11-01 13:58:46 -0200

    Add information login

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,23 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :gender, :birthday, :email, :fb_id, :avatar, :sign_in_count, :authentication_token, :created_at, :remember_created_at
+  attributes :id, 
+  					 :name, 
+  					 :gender, 
+  					 :birthday, 
+  					 :email, 
+  					 :fb_id, 
+  					 :avatar, 
+  					 :sign_in_count, 
+  					 :authentication_token, 
+  					 :created_at, 
+  					 :remember_created_at,
+  					 :is_visited_courses,
+  					 :is_new
+
+  def is_visited_courses
+  	object.visits.count > 0
+  end
+
+  def is_new
+  	!is_visited_courses
+  end
 end
